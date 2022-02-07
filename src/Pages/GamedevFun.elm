a33ee2132954c8d6ae674a7901deba95033dba6f
Rob Sokolowski <rpsoko@gmail.com>
2022-02-06 22:32:28 -0500

    wtf

--- src/Pages/GamedevFun.elm
+++ src/Pages/GamedevFun.elm
@@ -1,10 +1,6 @@
 module Pages.GamedevFun exposing (Model, Msg, page)
 
---import TypedSvg exposing (circle, svg)
---import TypedSvg.Attributes exposing (cx, cy, fill, r, stroke, strokeWidth, viewBox)
---import TypedSvg.Core exposing (Svg)
---import TypedSvg.Types exposing (Paint(..), px)
-
+import Browser.Events as Events
 import Color
 import Effect exposing (Effect)
 import Element as E exposing (..)
@@ -13,10 +9,16 @@ import Element.Border as Border
 import Element.Events exposing (..)
 import Element.Font as Font
 import Element.Input as Input
+import Game exposing (svgElements)
 import Gen.Params.GamedevFun exposing (Params)
+import Html exposing (Html)
+import Json.Decode as Decode
 import Page
 import Request
+import Set
 import Shared
+import TypedSvg exposing (svg)
+import TypedSvg.Attributes exposing (viewBox)
 import UI
 import View exposing (View)
 
@@ -36,12 +38,24 @@ page shared req =
 
 
 type alias Model =
-    {}
+    { downKeys : List String
+    , playerPos : PlayerPos
+    }
+
+
+type alias PlayerPos =
+    { x : Float
+    , y : Float
+    }
 
 
 init : ( Model, Effect Msg )
 init =
-    ( {}, Effect.none )
+    ( { downKeys = []
+      , playerPos = { x = 0, y = 0 }
+      }
+    , Effect.none
+    )
 
 
 
@@ -49,13 +63,62 @@ init =
 
 
 type Msg
-    = ReplaceMe
+    = KeyDowns KeyCode
+    | ClearPressed
+
+
+type Action
+    = MoveX Float
+    | MoveY Float
+
+
+type alias KeyCode =
+    String
+
+
+mapKeyCodeToAction : KeyCode -> Maybe Action
+mapKeyCodeToAction code =
+    if Set.member code <| Set.fromList [ "w", "W", "ArrowUp" ] then
+        Just <| MoveY -10
+
+    else if Set.member code <| Set.fromList [ "s", "S", "ArrowDown" ] then
+        Just <| MoveY 10
+
+    else
+        Nothing
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
+        KeyDowns code ->
+            let
+                action =
+                    mapKeyCodeToAction code
+
+                pos =
+                    model.playerPos
+
+                newPlayerPos =
+                    case action of
+                        Just action_ ->
+                            case action_ of
+                                MoveX dx ->
+                                    { pos | x = pos.x + dx }
+
+                                MoveY dy ->
+                                    { pos | y = pos.y + dy }
+
+                        Nothing ->
+                            pos
+            in
+            ( { downKeys = []
+              , playerPos = newPlayerPos
+              }
+            , Effect.none
+            )
+
+        ClearPressed ->
             ( model, Effect.none )
 
 
@@ -65,7 +128,15 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ Events.onKeyDown (Decode.map KeyDowns keyDecoder)
+        , Events.onKeyUp (Decode.succeed ClearPressed)
+        ]
+
+
+keyDecoder : Decode.Decoder String
+keyDecoder =
+    Decode.field "key" Decode.string
 
 
 
@@ -77,29 +148,12 @@ view model =
     { title = "GameDev Fun"
     , body =
         [ layout
-            [ width fill
-            , height fill
+            [ width E.fill
+            , height E.fill
             ]
             (elements model)
         ]
     }
-
-
-
---svgAsElement : Element Msg
---svgAsElement =
---    layout [] myCircle
---myCircle : Svg msg
---myCircle =
---    circle
---        [ cx (px 100)
---        , cy (px 100)
---        , r (px 30)
---        , fill <| Paint Color.blue
---        , strokeWidth (px 2)
---        , stroke <| Paint <| Color.rgba 0.8 0 0 0.5
---        ]
---        []
 
 
 borderedRow : List (Element Msg) -> Element Msg
@@ -118,16 +172,31 @@ borderedRow =
         ]
 
 
-content : Element msg
-content =
+content : Model -> Element Msg
+content model =
+    let
+        viewPortWidth =
+            1000
+
+        viewPortHeight =
+            700
+
+        --svgLayout : { width : Float, height : Float } -> Html msg
+        --svgLayout viewPort =
+        --    svg [ viewBox 0 0 viewPort.width viewPort.height ] <| svgElements model
+    in
     row
-        [ width <| px 800
-        , height <| px 600
+        [ width <| E.px viewPortWidth
+        , height <| E.px viewPortHeight
         , centerX
         , centerY
         , Background.color UI.palette.white
+        , Border.width 5
+        , Border.color UI.palette.lightBlue
+        , Border.rounded 3
         ]
-        [ text "stuff"
+        [ --E.html <| svgLayout { height = viewPortHeight, width = viewPortWidth }
+          text "HAllo"
         ]
 
 
@@ -137,7 +206,7 @@ elements model =
         header : Element msg
         header =
             row
-                [ width fill
+                [ width E.fill
                 , padding 10
                 , spacing 10
                 , Background.color UI.palette.lightGrey
@@ -152,8 +221,8 @@ elements model =
         logo : Element msg
         logo =
             el
-                [ width <| px 80
-                , height <| px 40
+                [ width <| E.px 80
+                , height <| E.px 40
                 , Border.width 2
                 , Border.rounded 6
                 , Border.color UI.palette.blue
@@ -169,7 +238,7 @@ elements model =
         footer : Element msg
         footer =
             row
-                [ width fill
+                [ width E.fill
                 , padding 5
                 , Background.color UI.palette.lightGrey
                 , Border.widthEach { top = 1, bottom = 0, left = 0, right = 0 }
@@ -185,13 +254,13 @@ elements model =
                 ]
     in
     E.column
-        [ width fill
-        , height fill
+        [ width E.fill
+        , height E.fill
         , Background.color UI.palette.darkCharcoal
         , Font.size 12
         ]
     <|
         [ header
-        , content
+        , content model
         , footer
         ]
