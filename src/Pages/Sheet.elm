0382ecaf3582a2f4121c9df83d04a1730fdcd32a
Rob Sokolowski <993431+rob-sokolowski@users.noreply.github.com>
2022-07-11 12:42:17 -0400

    try resizing idea (#22)

--- src/Pages/Sheet.elm
+++ src/Pages/Sheet.elm
@@ -63,6 +63,16 @@ type alias TimelineState =
     }
 
 
+type
+    RenderStatus
+    -- Since a lot of stuff is being rendered, initialize model to be Awaiting, and start the task of fetching
+    -- browser viewport size. Once received, this will be flipped to Ready, which then renders
+    -- This avoids the "double paint" problem on initial page loads (first paint assumes default screen size, followed
+    -- by a correct paint)
+    = AwaitingDomInfo
+    | Ready
+
+
 type alias Model =
     { sheetData : SheetData
     , keysDown : Set KeyCode
@@ -77,6 +87,7 @@ type alias Model =
     , fileUploadStatus : FileUploadStatus
     , nowish : Maybe Posix
     , viewport : Maybe Browser.Dom.Viewport
+    , renderStatus : RenderStatus
     }
 
 
@@ -107,6 +118,7 @@ type Timeline
 type Msg
     = Tick Posix
     | GotViewport Browser.Dom.Viewport
+    | GotResizeEvent Int Int
     | KeyWentDown KeyCode
     | KeyReleased KeyCode
     | ClickedCell CellCoords
@@ -225,6 +237,7 @@ init =
             , nowish = Nothing
             , viewport = Nothing
             , duckDbTableRefs = NotAsked
+            , renderStatus = AwaitingDomInfo
             }
     in
     ( model
@@ -309,8 +322,14 @@ update msg model =
         Tick now ->
             ( { model | nowish = Just now }, Effect.none )
 
+        GotResizeEvent _ _ ->
+            -- rather than keeping two copies of this info in memory, chain a resize event
+            -- to the existing flow on first page render. This should avoid strange resizing
+            -- frames from being rendered.. at least I hope so!
+            ( { model | renderStatus = AwaitingDomInfo }, Effect.fromCmd (Task.perform GotViewport Browser.Dom.getViewport) )
+
         GotViewport viewport ->
-            ( { model | viewport = Just viewport }, Effect.none )
+            ( { model | viewport = Just viewport, renderStatus = Ready }, Effect.none )
 
         GotDuckDbTableRefsResponse response ->
             case response of
@@ -544,6 +563,7 @@ subscriptions model =
     Sub.batch
         [ contextualKeystrokes
         , Time.every 500 Tick
+        , Events.onResize GotResizeEvent
         ]
 
 
@@ -564,15 +584,20 @@ view model =
 
         elements : Model -> Element Msg
         elements mdl =
-            E.column
-                [ E.width E.fill
-                , E.height E.fill
-                , Background.color UI.palette.white
-                , Font.size 12
-                , padding 5
-                ]
-                [ content mdl
-                ]
+            case mdl.renderStatus of
+                AwaitingDomInfo ->
+                    E.none
+
+                Ready ->
+                    E.column
+                        [ E.width E.fill
+                        , E.height E.fill
+                        , Background.color UI.palette.white
+                        , Font.size 12
+                        , padding 5
+                        ]
+                        [ content mdl
+                        ]
 
         content : Model -> Element Msg
         content mdl =
