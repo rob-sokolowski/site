b1ef6418470c06fc6c4984c1672d273aaa6d6de3
Rob Sokolowski <rpsoko@gmail.com>
2022-07-13 15:30:27 -0400

    docker-compose steps, elm-spa compilation

--- scripts/build-ci-image.sh
+++ scripts/build-ci-image.sh
@@ -3,7 +3,9 @@
 # this script is intended for local debugging of build processes, see the .cloudbuild/ directory of this project
 # for the actual build scripts
 
+
+# tagging to gcr labeling for local testing.. don't push, let CloudBuild handle that!
 docker build \
-    -t site-ci-build-image:local-only \
+    -t gcr.io/fir-sandbox-326008/site-ci-builder:latest \
     --file Dockerfile-Ci \
     .
