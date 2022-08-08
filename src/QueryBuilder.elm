c4c05eabc022445f6808c78708d87d38c47d87f7
Rob Sokolowski <993431+rob-sokolowski@users.noreply.github.com>
2022-08-08 06:38:46 -0400

    Query building POC (#61)

---
+++ src/QueryBuilder.elm
@@ -0,0 +1,99 @@
+module QueryBuilder exposing (..)
+
+
+type alias ColumnRef =
+    String
+
+
+type KimballColumn
+    = KimballColumn_ ( ColumnRef, KimballClassification )
+
+
+type KimballClassification
+    = Dimension
+    | Measure Aggregation
+    | Time
+    | Error
+
+
+kimballClassificationToString : KimballClassification -> String
+kimballClassificationToString kc =
+    case kc of
+        Dimension ->
+            "dimension"
+
+        Measure _ ->
+            "measure"
+
+        Time ->
+            "time"
+
+        Error ->
+            "error"
+
+
+type Aggregation
+    = Sum
+    | Mean
+    | Median
+    | Min
+    | Max
+
+
+type alias SqlStr =
+    String
+
+
+type alias TableRef =
+    String
+
+
+queryBuilder : List KimballColumn -> TableRef -> SqlStr
+queryBuilder kCols tRef =
+    let
+        selectFields : List ColumnRef
+        selectFields =
+            List.map
+                (\kc ->
+                    case kc of
+                        KimballColumn_ ( colRef, _ ) ->
+                            colRef
+                )
+                (List.filter
+                    (\e ->
+                        case e of
+                            KimballColumn_ ( _, kClass ) ->
+                                case kClass of
+                                    Dimension ->
+                                        True
+
+                                    _ ->
+                                        False
+                    )
+                    kCols
+                )
+
+        groupByFields : List ColumnRef
+        groupByFields =
+            -- TODO: Tidy this (and above) up? Almost identical w/ Measure / Dim
+            List.map
+                (\kc ->
+                    case kc of
+                        KimballColumn_ ( colRef, _ ) ->
+                            colRef
+                )
+                (List.filter
+                    (\e ->
+                        case e of
+                            KimballColumn_ ( _, kClass ) ->
+                                case kClass of
+                                    Measure _ ->
+                                        True
+
+                                    _ ->
+                                        False
+                    )
+                    kCols
+                )
+    in
+    "select " ++ String.join "," selectFields ++ "," ++ String.join "," groupByFields ++ " from " ++ tRef
