436a9ccc69dc098959f494ce54cca4b230f4082e
Rob Sokolowski <993431+rob-sokolowski@users.noreply.github.com>
2023-12-01 08:57:55 -0500

    Bball tree time (#99)

---
+++ src/TimeSplitList.elm
@@ -0,0 +1,133 @@
+module TimeSplitList exposing
+    ( Next(..)
+    , Node(..)
+    , TimeSplitList
+    , newTimeSplitList
+    , tailOf
+    )
+
+-- This TimeSplitList is intended to support the time-splitting animations of the bouncing ball applet. As of writing,
+-- I'm not entirely sure to name it, the working title is "TimeSplitList"
+--
+-- Features this needs to support:
+-- 1) Clock runs without any disruption
+-- 2) Clock runs without disruption, is re-winded to some frame prior, and animation is resumed
+-- 3) Clock runs, intervention is proposed, two timelines proceed, one undisturbed, one disturbed exactly once (the intervention)
+-- 4) Clock runs, intervention is proposed, two timelines proceed, one undisturbed, one disturbed. Another intervention is
+--    proposed on the disturbed timeline resulting in a total of 3 timelines, labeled 0, 1, & 2:
+--
+--                           /-----|| 2
+--                  /-------x------|| 1
+--       |---------x---------------|| 0
+--
+-- 5) Repeat process of step 4, then rewind to some point between the first and second intervention. A 3rd intervention is
+--    proposed at this instant, on the control timeline. The result is, note how there is still exactly 1 undisturbed
+--    timeline, 0, and a new timeline, labeled 3:
+--
+--                           /-----|| 2
+--                  /-------x------|| 1
+--                 |   /-----------|| 3
+--       |---------x---x-----------|| 0
+--
+
+
+type alias TreeListErr =
+    String
+
+
+type alias TimelineIx =
+    Int
+
+
+type alias FrameIx =
+    Int
+
+
+type alias TimeSplitList a =
+    { head : Node a
+    , timelineCount : Int
+    }
+
+
+type Node a
+    = Node_
+        { ix : FrameIx
+        , timelineIx : TimelineIx
+        , val : a
+        , next : Maybe (Next a)
+        }
+
+
+type Next a
+    = Seq (Node a)
+    | Split ( Node a, Node a )
+
+
+defaultNode =
+    -- TODO: placeholder value until I solve degenerate cases, and wrap things in a Result
+    Node_
+        { ix = 0
+        , timelineIx = 0
+        , val = "One"
+        , next = Nothing
+        }
+
+
+{-| Returns a newly initialized TimeSplitList, with exactly 1 node with val, and 1 timeline
+-}
+newTimeSplitList : a -> TimeSplitList a
+newTimeSplitList val =
+    { head =
+        Node_
+            { ix = 0
+            , timelineIx = 0
+            , val = val
+            , next = Nothing
+            }
+    , timelineCount = 1
+    }
+
+
+{-| tailOf a timelineIx, given some node
+
+TODO: tail along a given TimelineIx?
+TODO: tail along Set TimelineIx?
+TODO: Result and Err instead of Maybe for return type
+TODO: Maybe I should have tailOf vs tails???
+TODO: elm-review rule for stack safety
+
+-}
+tailOf : Node a -> TimelineIx -> Maybe (Node a)
+tailOf (Node_ node) tlix =
+    case node.next of
+        Just next ->
+            case next of
+                Seq next_ ->
+                    tailOf next_ tlix
+
+                Split ( Node_ lhs, Node_ rhs ) ->
+                    if lhs.timelineIx == tlix then
+                        tailOf (Node_ lhs) tlix
+
+                    else if rhs.timelineIx == tlix then
+                        tailOf (Node_ rhs) tlix
+
+                    else
+                        Nothing
+
+        Nothing ->
+            Just (Node_ node)
+
+
+append : TimeSplitList a -> List (Node a) -> Result TreeListErr (Node a)
+append tree nodes =
+    if tree.timelineCount == List.length nodes then
+        Result.Err "How do I do this?!?!?"
+
+    else
+        Result.Err "When appending to a TimeSplitList, the number of nodes to be append must equal the number of timelines"
+
+
+appendSub : TimeSplitList a -> TimelineIx -> Node a -> Result TreeListErr (Node a)
+appendSub tree tx node =
+    Result.Err "TODO!"
---
+++ tests/TimeSplitListTest.elm
@@ -0,0 +1,138 @@
+module TimeSplitListTest exposing (..)
+
+import Expect
+import Test exposing (..)
+import TimeSplitList exposing (Next(..), Node(..), TimeSplitList, newTimeSplitList, tailOf)
+
+
+suite : Test
+suite =
+    describe "Tree List data structure"
+        [ describe "Creating, appending, splitting, etc."
+            [ test "Initialize new TimeSplitList"
+                (\_ ->
+                    newTimeSplitList "One"
+                        |> Expect.equal
+                            initTimeSplitListOfString
+                )
+            , test "tailOf simpleTwoTimelineTreeList - timelineIx=0"
+                (\_ ->
+                    tailOf simpleTwoTimelineTreeList.head 0
+                        |> Expect.equal
+                            (Just <|
+                                Node_
+                                    { timelineIx = 0
+                                    , ix = 3
+                                    , val = "0-3"
+                                    , next = Nothing
+                                    }
+                            )
+                )
+            , test "tailOf simpleTwoTimelineTreeList - timelineIx=1"
+                (\_ ->
+                    tailOf simpleTwoTimelineTreeList.head 1
+                        |> Expect.equal
+                            (Just <|
+                                Node_
+                                    { timelineIx = 1
+                                    , ix = 3
+                                    , val = "1-3"
+                                    , next = Nothing
+                                    }
+                            )
+                )
+            , test "tailOf simpleTwoTimelineTreeList - timelineIx=100"
+                -- TODO: This is returning Nothing, but is it doing so for the right reasons?
+                (\_ ->
+                    tailOf simpleTwoTimelineTreeList.head 100
+                        |> Expect.equal Nothing
+                )
+            ]
+        ]
+
+
+initTimeSplitListOfString : TimeSplitList String
+initTimeSplitListOfString =
+    { head =
+        Node_
+            { ix = 0
+            , timelineIx = 0
+            , val = "One"
+            , next = Nothing
+            }
+    , timelineCount = 1
+    }
+
+
+{-| Create the following TimeSplitList for some test cases:
+
+     /-- 1
+    -x-- 0
+
+That is, there are 4 frames, with ixes; 0, 1, 2, 3
+And two timelines with ixes; 0, 1
+
+Timeline 0 splits on frameIx 1, with its first independent frame
+starting on timeIx 2, continuing onto timeIx 3
+
+-}
+simpleTwoTimelineTreeList : TimeSplitList String
+simpleTwoTimelineTreeList =
+    let
+        node_0_0 : Node String
+        node_0_0 =
+            Node_
+                { timelineIx = 0
+                , ix = 0
+                , val = "0-0"
+                , next = Just (Seq node_0_1)
+                }
+
+        node_0_1 : Node String
+        node_0_1 =
+            Node_
+                { timelineIx = 0
+                , ix = 1
+                , val = "0-1"
+                , next = Just (Split ( node_0_2, node_1_2 ))
+                }
+
+        node_0_2 : Node String
+        node_0_2 =
+            Node_
+                { timelineIx = 0
+                , ix = 2
+                , val = "0-2"
+                , next = Just (Seq node_0_3)
+                }
+
+        node_1_2 : Node String
+        node_1_2 =
+            Node_
+                { timelineIx = 1
+                , ix = 2
+                , val = "1-2"
+                , next = Just (Seq node_1_3)
+                }
+
+        node_0_3 : Node String
+        node_0_3 =
+            Node_
+                { timelineIx = 0
+                , ix = 3
+                , val = "0-3"
+                , next = Nothing
+                }
+
+        node_1_3 : Node String
+        node_1_3 =
+            Node_
+                { timelineIx = 1
+                , ix = 3
+                , val = "1-3"
+                , next = Nothing
+                }
+    in
+    { head = node_0_0
+    , timelineCount = 2
+    }
