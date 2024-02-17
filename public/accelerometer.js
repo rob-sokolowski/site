a1f3e5d1fe004bcf0e0e9d3884a099e50ed6e653
Rob Sokolowski <993431+rob-sokolowski@users.noreply.github.com>
2024-02-17 09:14:25 -0500

    stash.. wiring up export

--- public/accelerometer.js
+++ public/accelerometer.js
@@ -1,30 +1,32 @@
-// Check if DeviceOrientationEvent is supported by the browser
-if ('DeviceOrientationEvent' in window) {
-    // Request permission for iOS 13+ devices
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-        DeviceOrientationEvent.requestPermission()
-            .then(permissionState => {
-                if (permissionState === 'granted') {
-                    window.addEventListener('deviceorientation', handleOrientationEvent, true);
-                } else {
-                    console.log('Permission to access device orientation was denied.');
-                }
-            })
-            .catch(console.error);
-    } else {
-        // Non-iOS 13+ devices do not need permission
-        window.addEventListener('deviceorientation', handleOrientationEvent, true);
+const initAccelerometer = async (app) => {
+    const handleOrientationEvent = (event) => {
+        const { alpha, beta, gamma } = event;
+
+        console.log(`Rotation around Z-axis (alpha): ${alpha}`);
+        // console.log(`Rotation around X-axis (beta): ${beta}`);
+        // console.log(`Rotation around Y-axis (gamma): ${gamma}`);
+
+        app.ports.markLoadVegaSequenceFinished.send(0);
     }
-} else {
-    console.log('DeviceOrientationEvent is not supported by your browser.');
-}
 
-function handleOrientationEvent(event) {
-    const { alpha, beta, gamma } = event;
-
-    console.log(`Rotation around Z-axis (alpha): ${alpha}`);
-    // console.log(`Rotation around X-axis (beta): ${beta}`);
-    // console.log(`Rotation around Y-axis (gamma): ${gamma}`);
-}
-
-console.log("accelerometer.js loaded successfully");
+    // Check if DeviceOrientationEvent is supported by the browser
+    if ('DeviceOrientationEvent' in window) {
+        // Request permission for iOS 13+ devices
+        if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+            DeviceOrientationEvent.requestPermission()
+                .then(permissionState => {
+                    if (permissionState === 'granted') {
+                        window.addEventListener('deviceorientation', handleOrientationEvent, true);
+                    } else {
+                        console.log('Permission to access device orientation was denied.');
+                    }
+                })
+                .catch(console.error);
+        } else {
+            // Non-iOS 13+ devices do not need permission
+            window.addEventListener('deviceorientation', handleOrientationEvent, true);
+        }
+    } else {
+        console.log('DeviceOrientationEvent is not supported by your browser.');
+    }
+};
